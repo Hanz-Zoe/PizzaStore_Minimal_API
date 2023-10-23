d6bc6b3d79d505f4dde4436911f00068537c63cc
Vinícius Sobral <vsobralima@gmail.com>
2023-10-22 21:31:25 -0300

    "PizzaStore API V1.0"

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using PizzaStore.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making Pizzas you love", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API v1");
+});
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/Pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.Run();
